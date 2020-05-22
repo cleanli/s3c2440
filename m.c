ef90374bad96f6a0122217fa01f4a07e4c14d5d8
clean_li <clean_li@yahoo.com>
2020-05-22 22:12:44 +0800

    draw sqrt curve

--- m.c
+++ m.c
@@ -939,7 +939,7 @@ void Lcd_Tft_320X240_Init_from_uboot( void )
 
 r:
     for(x = 0; x<320; x++){
-        y=x/2;
+        y=sqrt(x)*10.0f;
         PutPixel(x,y,0x7e0);
         draw_sq(x,y, x+20, y+20, 0x7e0);
         delay(10);
@@ -947,7 +947,7 @@ r:
         draw_sq(x,y, x+20, y+20, 0xf81f);
     }
     for(x = 320; x>0; x--){
-        y=sqrt(x);
+        y=x/2;
         PutPixel(x,y,0x7e0);
         draw_sq(x,y, x+20, y+20, 0x7e0);
         delay(10);
