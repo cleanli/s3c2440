ecb5ea133ad1043cbe2ac43b9fd8c238bf2ddb49
clean_li <clean_li@yahoo.com>
2020-05-30 23:40:43 +0800

    porting dm9000 A part

--- type.h
+++ type.h
@@ -3,6 +3,25 @@
 #include<stdint.h>
 
 typedef uint32_t uint;
+typedef uint32_t u32;
+typedef uint32_t ulong;
+typedef uint16_t u16;
+typedef uint16_t ushort;
+typedef uint8_t u8;
+typedef uint8_t uchar;
 typedef void (*interrupt_func)(void);
 
+typedef struct bd_info {
+    int			bi_baudrate;	/* serial console baudrate */
+    unsigned long	bi_ip_addr;	/* IP Address */
+    struct environment_s	       *bi_env;
+    ulong	        bi_arch_number;	/* unique id for this board */
+    ulong	        bi_boot_params;	/* where this board expects params */
+    struct				/* RAM configuration */
+    {
+	ulong start;
+	ulong size;
+    }			
+    bi_dram[4];
+} bd_t;
 #endif
